61fc854c36a2fcb22bb32e1324322bb8d602d7af
mongilteacher <mongilteacher@gmail.com>
2026-01-26 14:13:10 +0900

    Feat: 클릭

--- Assets/01.Scripts/Ingame/Click/ClickTarget.cs
+++ Assets/01.Scripts/Ingame/Click/ClickTarget.cs
@@ -1,35 +1,14 @@
 using UnityEngine;
 
-public class ClickTarget : MonoBehaviour
+public class ClickTarget : MonoBehaviour, Clickable
 {
-    // 목적: 타겟을 클릭하면 클릭하고 싶다.
-    public LayerMask ClickLayer;
-    
-    private void Update()
-    {
-        // 1. 마우스 클릭을 감지한다.
-        if (Input.GetMouseButtonDown(0))
-        {        
-            // 2. 마우스 좌표를 구한다.
-            // 마우스 좌표계는 스크린 좌표계
-            Vector2 mousePos = Input.mousePosition;
-            
-            // 마우스의 스크린 좌표계를 월드 좌표계로 바꿔줄 필요가 있다.
-            Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-            
-            // 3. 마우스 좌표로 가상의 레이저를 쏴서 그 레이저가 클릭타겟과 충돌했는지 체크
-            RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero, 0f, ClickLayer);
-            if (hit == true)
-            {
-                Debug.Log("다음부터는 늦지 않겠습니다.");
-            }
-            
-            // 4. 게임 오브젝트 설정
-            // - ClickTarget이라는 Layer 추가
-            // - Layer 설정 (인스펙터)
-        }
+   [SerializeField] private string _name;
 
-        
-        
-    }
+
+   public bool OnClick()
+   {
+      Debug.Log($"{_name}: 다음부터는 늦지 않겠습니다.");
+
+      return true;
+   }
 }
---
+++ Assets/01.Scripts/Ingame/Click/Clicker.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    // 목적: 타겟을 클릭하면 클릭하고 싶다.
+    public LayerMask ClickLayer;
+    
+    
+    private void Update()
+    {
+        // 1. 마우스 클릭을 감지한다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 2. 마우스 좌표를 구한다.
+            // 마우스 좌표계는 스크린 좌표계
+            Vector2 mousePos = Input.mousePosition;
+            Click(mousePos);
+        }
+    }
+
+    private void Click(Vector2 mousePos)
+    {
+        // 마우스의 스크린 좌표계를 월드 좌표계로 바꿔줄 필요가 있다.
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        // 3. 마우스 좌표로 가상의 레이저를 쏴서 그 레이저가 클릭타겟과 충돌했는지 체크
+        RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+        if (hit == true)
+        {
+            // 현재씨를 때릴떄는 OnCLick
+            Clickable clickable = hit.collider.GetComponent<Clickable>();
+            clickable?.OnClick();
+        }
+    }
+}
